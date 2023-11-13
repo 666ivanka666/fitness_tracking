bcc0f72eb46f5417dd79580eae81c8b504b99fa7
ivanka <xivankax@gmail.com>
2023-11-13 18:04:22 +0100

    zadnja promjena

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,8 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { NutritionModule } from './nutrition/nutrition.module';
 import { TrainingModule } from './training/training.module';
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
 
 @Module({
   imports: [UserModule, TrainingModule, NutritionModule],
-  providers: [AppService],
-  controllers: [AppController],
 })
 export class AppModule {}
